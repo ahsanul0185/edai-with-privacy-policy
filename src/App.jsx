27a3d3a7c59bd17532bc4327e3a9167e7bcb04e4
unknown <ahsanul0185@gmail.com>
2025-03-10 20:34:35 +0600

    fixed scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
+import ScrollToTop from './utils/ScrollToTop'
 
 const App = () => {
   return (
     <div>
+        <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/privacy-policy' element={<PrivacyPolicy />} />
